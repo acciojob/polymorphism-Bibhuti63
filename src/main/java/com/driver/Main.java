be4ff9ce538acec4d7ba8c414691ed717dbc0131
Bibhuti <biswalbibhuti8@gmail.com>
2023-01-13 10:24:10 +0530

    updated main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,17 +7,17 @@ public class Main {
         p.product(10, 20, 30);
         p.product(10.0, 20.0);
     }
+    class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 
     
-}
-class Product{
-    public int product(int x, int y) {
-        return x*y;
-    }
-    public int product(int x, int y, int z) {
-        return x*y*z;
-    }
-    public double product(double x, double y) {
-        return x*y;
-    }
 }
